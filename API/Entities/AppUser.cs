3d82b5e245b1bc6b8fdfbe4ac2299b076d1ebd7b
Abril D'Angelo <dangelolebrero.abril@gmail.com>
2024-08-26 18:23:24 -0300

    feat: add entity framework and create db

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    // Entity framework can only access public properties
+    // Id will be the primary key. If there's no Id, you have to specify he primary key using [key]
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
+ 
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f9f635dcfb511f4242412c1279d0810d96d36f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
